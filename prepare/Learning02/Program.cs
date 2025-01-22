c56ebfb02b593dc0fe7e8b76c837506249dc404a
ZMRTZER26 <zmrtzer@byui.edu>
2025-01-22 14:12:56 -0700

    W03 Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "O.W.C.A.";
+        job1._startYear = 2007;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Janitor";
+        job2._company = "Doofenshmirtz Evil Incorporated";
+        job2._startYear = 2007;
+        job2._endYear = 2015;
+
+        Resume myResume = new Resume();
+        myResume._name = "Henchman #1";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
